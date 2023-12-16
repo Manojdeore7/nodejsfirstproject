f2cd4c1b66419309c1a9534cde35f47e8d32c904
Manojdeore7 <manojddeore11>
2023-12-16 09:42:17 +0530

    done first task

---
+++ index.js
@@ -0,0 +1,9 @@
+// let http = require("http");
+// http
+//   .createServer((req, res) => {
+//     console.log("server is working");
+//     res.end("HI from server");
+//   })
+//   .listen(8000);
+let fs = require("fs");
+fs.writeFileSync("m.text", "haSGD");
