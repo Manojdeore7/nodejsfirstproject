fe1432ff0dea0203d9f8acebed372d6bad1f7c49
Manojdeore7 <manojddeore11>
2023-12-17 22:05:45 +0530

    add first express work

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-// let http = require("http");
-// http
-//   .createServer((req, res) => {
-//     console.log("server is working");
-//     res.end("HI from server");
-//   })
-//   .listen(8000);
-let fs = require("fs");
-fs.writeFileSync("m.text", "haSGD");
+let express = require("express");
+let app = express();
+
+app.use((req, res, next) => {
+  console.log("I am in first middle ware");
+  next();
+});
+
+app.use((req, res) => {
+  console.log("I am in second middle ware");
+  res.set("Content-Type", "text/html");
+  res.send("<h1>ji</h1>");
+});
+app.listen(3000);
