fdb84dff5f63350e0aea165bb03339c06ad806f5
Manojdeore7 <manojddeore11>
2023-12-19 10:12:36 +0530

    attached the pblic folder

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 let express = require("express");
 let adminPage = require("./Routes/Admin");
+let path = require("path");
 let HomePage = require("./Routes/Home");
 let app = express();
 let fs = require("fs");
 let bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(express.static(path.join(__dirname, "HtmlFiles"))); //for make working of css
 app.use(adminPage);
 app.use(HomePage);
 app.use("/", (req, res) => {
-  res.status(404).send("Page is not Found");
+  res.status(404).sendFile(path.join(__dirname, "./HtmlFiles/404.html"));
 });
 app.listen(7000);
